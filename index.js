3ef6a7e9110ab741ac3b67d35ba026ee2a7181a1
Tommy Grullon <tommycontreras34@gmail.com>
2026-07-30 09:39:28 -0400

    chore: initialize project with package.json and add express and nodemon as dependencies

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello World")
+import express from "express"
+
+const PORT = 3000
+
+const app = express()
+
+app.get("/health", (_req, res) => {
+    res.status(200).json({ healthy: true })
+})
+
+app.listen(PORT, () => console.log(`The server is running on port: ${PORT}`))
