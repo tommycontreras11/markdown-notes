0102e044a7cdc3dac39cca1d41465877fe7754c1
Tommy Grullon <tommycontreras34@gmail.com>
2026-08-17 16:17:53 -0400

    feat: :sparkles: add cors to allow origin from the 3001 port frontend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,20 @@
 import express from "express";
+import cors from "cors";
 import { initFile } from "./helper/file.js";
-import routes from "./routes/index.js"
+import routes from "./routes/index.js";
 
 const PORT = 3000;
 
 const app = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:3001",
+  }),
+);
+
 app.use(express.json());
-app.use(routes)
+app.use(routes);
 
 await initFile();
 
