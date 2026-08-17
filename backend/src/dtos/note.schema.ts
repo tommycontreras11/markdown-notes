c0ced960c3fe3c5204393bd279fbf88d44abbc77
Tommy Grullon <tommycontreras34@gmail.com>
2026-08-17 15:28:13 -0400

    feat: :sparkles: add schema to validate the note grammar

--- backend/src/dtos/note.schema.ts
+++ backend/src/dtos/note.schema.ts
@@ -4,3 +4,6 @@ export const createNoteSchema = z.object({
     content: z.string({ error: "Content is required" }).trim().min(1, { error: "The min length is 1" })
 })
 
+export const checkNoteGrammarSchema = z.object({
+    content: z.string({ error: "Content is required" }).trim().min(1, { error: "The min length is 1" })
+})
--- backend/src/routes/api/note/index.ts
+++ backend/src/routes/api/note/index.ts
@@ -2,9 +2,12 @@ import { Request, Response } from "express";
 import { Router } from "express";
 import { noteService } from "../../../services/note/index.service.js";
 import { validateDto } from "../../../middlewares/validate-dto.middleware.js";
-import { createNoteSchema } from "../../../dtos/note.schema.js";
+import {
+  checkNoteGrammarSchema,
+  createNoteSchema,
+} from "../../../dtos/note.schema.js";
 
-const router = Router()
+const router = Router();
 
 router.get("/", async (_req: Request, res: Response) => {
   const notes = await noteService.getAll();
@@ -13,48 +16,46 @@ router.get("/", async (_req: Request, res: Response) => {
 });
 
 router.get("/render/:id", async (req: Request, res: Response) => {
-  const { id } = req.params
+  const { id } = req.params;
 
-  const note = await noteService.getById(+id)
+  const note = await noteService.getById(+id);
 
-  if(!note) return res.status(400).json({ error: { message: "Note not found" } })
+  if (!note)
+    return res.status(400).json({ error: { message: "Note not found" } });
 
   return res.status(200).json({ data: note });
 });
 
+router.post(
+  "/",
+  validateDto(createNoteSchema),
+  async (req: Request, res: Response) => {
+    const { content } = req.body;
 
-router.post("/", validateDto(createNoteSchema), async (req: Request, res: Response) => {
-  const { content } = req.body;
+    await noteService.create(content);
 
-  if (!content)
-    return res
-      .status(400)
-      .json({ error: { message: "The content cannot be empty" } });
+    return res.status(201).json({ message: "Note saved successfully" });
+  },
+);
 
-    await noteService.create(content)
+router.post(
+  "/check-grammar",
+  validateDto(checkNoteGrammarSchema),
+  async (req: Request, res: Response) => {
+    const { content } = req.body;
 
-  return res.status(200).json({ message: "Note saved successfully" });
-});
+    try {
+      const result = await noteService.checkGrammar(content);
 
-router.post("/check-grammar", async (req: Request, res: Response) => {
-  const { content } = req.body;
+      return res.status(200).json({ data: result });
+    } catch (error) {
+      return res.status(503).json({
+        error: {
+          message: "Grammar service unavailable",
+        },
+      });
+    }
+  },
+);
 
-  if (!content)
-    return res
-      .status(400)
-      .json({ error: { message: "The content cannot be empty" } });
-
-  try {
-    const result = await noteService.checkGrammar(content);
-
-    return res.status(200).json({ result });
-  } catch (error) {
-    return res.status(503).json({
-      error: {
-        message: "Grammar service unavailable",
-      },
-    });
-  }
-});
-
-export default router
+export default router;
